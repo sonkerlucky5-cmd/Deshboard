66562987ddd8f091e84e28f8f9786d9ba76b9405
sonkerlucky5-cmd <224416154+sonkerlucky5-cmd@users.noreply.github.com>
2026-04-05 14:09:26 +0530

    Finance Dashboard
    A simple and clean finance dashboard built using React.js to track transactions, analyze spending, and view financial insights.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('recharts')) {
+              return 'recharts';
+            }
+            if (id.includes('lucide-react')) {
+              return 'lucide';
+            }
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
+              return 'vendor';
+            }
+          }
+        }
+      }
+    }
+  }
 })
